f87a6943a52aa91e08b29e0effbf63b123c5a71b
Yan Carlos <urbanoyaan@gmail.com>
2023-05-15 00:14:34 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/html/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'salmon-page': '#fa704a',
+        'blue-page': '#3d85c6',
+      },
+    },
+  },
+  plugins: [],
+}
+
